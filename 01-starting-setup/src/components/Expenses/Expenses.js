16f6b04ad5dbebd8eb6ee3e80a59d0f1cf42f4e9
ksivabalan <ksivabalan.91@gmail.com>
2023-10-01 14:00:09 +0800

    Organizing components

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import './Expenses.css'
-import Card from './Card';
+import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 
 function Expenses(props){
@@ -7,12 +7,14 @@ function Expenses(props){
     const item1 = props.expenses[0]
     const item2 = props.expenses[1]
     const item3 = props.expenses[2]
+    const item4 = props.expenses[3]
 
     return(
         <Card className="expenses">
             <ExpenseItem title={item1.title} amount={item1.amount} date = {item1.date}/>
             <ExpenseItem title={item2.title} amount={item2.amount} date = {item2.date}/>
             <ExpenseItem title={item3.title} amount={item3.amount} date = {item3.date}/>
+            <ExpenseItem title={item4.title} amount={item4.amount} date = {item4.date}/>
         </Card>
     );
 }
