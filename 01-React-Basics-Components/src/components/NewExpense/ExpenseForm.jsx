8d6f7155babad609160eb426d9d19d779177a0e4
ksivabalan <ksivabalan.91@gmail.com>
2023-10-02 21:30:11 +0800

    form submission

--- 01-React-Basics-Components/src/components/NewExpense/ExpenseForm.jsx
+++ 01-React-Basics-Components/src/components/NewExpense/ExpenseForm.jsx
@@ -10,30 +10,33 @@ export default function ExpenseForm() {
     const amountChangeHandler = (event) => { setEnteredAmount(event.target.value) };
     const dateChangeHandler = (event) => { setEnteredDate(event.target.value) };
 
-    const inputChangeHandler = (identifier, value) => {
-        if (identifier === 'title') {
-            setEnteredTitle(value)
-        } else if (identifier === 'amount') {
-            setEnteredAmount(value)
-        } else if (identifier === 'date') {
-            setEnteredDate(value)
-        }
-    }   
+    const submitHandler = (event) => {
+        // by default, submitting forms will reload page. Hence we can stop the reload with the following function
+        event.preventDefault();
+
+        const expenseData = {
+            title: event.target.title.value,
+            amount: event.target.amount.value,
+            date: new Date(event.target.date.value)
+        };
+        console.log(expenseData)
+    };
+    
 
     return (
-        <form >
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label htmlFor="">Title</label>
-                    <input value={enteredTitle} type="text" onChange={(event)=>{inputChangeHandler('title',event.target.value) }} />
+                    <input value={enteredTitle} type="text" onChange={titleChangeHandler} id='title'/>
                 </div>
                 <div className='new-expense__control'>
                     <label htmlFor="">Amount</label>
-                    <input value={enteredAmount} type="number" min="0.01" step="0.01" onChange={event=>inputChangeHandler('amount',event.target.value)} />
+                    <input value={enteredAmount} type="number" min="0.01" step="0.01" onChange={amountChangeHandler} id='amount'/>
                 </div>
                 <div className='new-expense__control'>
                     <label htmlFor="">Date</label>
-                    <input value={enteredDate} type="date" min="2019-01-01" max="2022-12-31" onChange={event=>inputChangeHandler('date',event.target.value)} />
+                    <input value={enteredDate} type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} id='date'/>
                 </div>
                 <div className='new-expense__actions'>
                     <button type="submit">Add Expense</button>
