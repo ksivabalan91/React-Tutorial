bf263bebff40f92a78351cd477067f04a4eec1c6
ksivabalan <ksivabalan.91@gmail.com>
2023-10-02 20:26:46 +0800

    Use prevState to update instead

--- 01-React-Basics-Components/src/components/NewExpense/ExpenseForm.jsx
+++ 01-React-Basics-Components/src/components/NewExpense/ExpenseForm.jsx
@@ -3,48 +3,54 @@ import './ExpenseForm.css'
 
 export default function ExpenseForm() {
     const [userInput, setUserInput] = useState({
-            enteredTitle: '',
-            enteredAmount:'',
-            enteredDate:''
-        });
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: ''
+    });
     const titleChangeHandler = (event) => {
-        setUserInput({
-            ...userInput, // spread the data
-            enteredTitle : event.target.value // override the title value
+        // use previous state to update the new state
+        setUserInput((prevState) => {
+            return { ...prevState, enteredTitle: event.target.value };
         })
     };
     const amountChangeHandler = (event) => {
-        setUserInput({
-            ...userInput, // spread the data
-            enteredAmount : event.target.value // override the title value
+        // use previous state to update the new state
+        setUserInput((prevState) => {
+            return {
+                ...prevState, // spread the data
+                enteredAmount: event.target.value // override the title value
+            }
         })
     };
     const dateChangeHandler = (event) => {
-        setUserInput({
-            ...userInput, // spread the data
-            enteredDate : event.target.value // override the title value
+        // use previous state to update the new state
+        setUserInput(prevState => {
+            return {
+                ...prevState, // spread the data
+                enteredDate: event.target.value // override the title value
+            }
         })
     };
 
     return (
-    <form>        
-        <div className='new-expense__controls'>
-            <div className='new-expense__control'>
-                <label htmlFor="">Title</label>
-                <input value={userInput.enteredTitle} type="text" onChange={titleChangeHandler}/>
+        <form>
+            <div className='new-expense__controls'>
+                <div className='new-expense__control'>
+                    <label htmlFor="">Title</label>
+                    <input value={userInput.enteredTitle} type="text" onChange={titleChangeHandler} />
+                </div>
+                <div className='new-expense__control'>
+                    <label htmlFor="">Amount</label>
+                    <input value={userInput.enteredAmount} type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+                </div>
+                <div className='new-expense__control'>
+                    <label htmlFor="">Date</label>
+                    <input value={userInput.enteredDate} type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+                </div>
+                <div className='new-expense__actions'>
+                    <button type="submit">Add Expense</button>
+                </div>
             </div>
-            <div className='new-expense__control'>
-                <label htmlFor="">Amount</label>
-                <input value={userInput.enteredAmount} type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
-            </div>
-            <div className='new-expense__control'>
-                <label htmlFor="">Date</label>
-                <input value={userInput.enteredDate} type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
-            </div>
-            <div className='new-expense__actions'>
-                <button type="submit">Add Expense</button>
-            </div>
-        </div>
-    </form>
-  )
+        </form>
+    )
 }
