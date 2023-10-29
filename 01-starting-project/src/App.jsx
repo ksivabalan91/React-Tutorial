f348fb6a4e9bb8abd1c46012b199b50542190cf0
ksivabalan <ksivabalan.91@gmail.com>
2023-10-29 13:06:53 +0800

    inject dynamic element into html code

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,14 +1,28 @@
+const reactDescriptions = ['Fundamental','Crucial','Core'];
+
+function getRandom(max){
+  return (Math.floor(Math.random()*(max+1)))
+}
+
+function Header() {
+
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {reactDescriptions[getRandom(3)]} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  )
+}
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      {/* You can use the following </> syntax if there is no additional classes and 
+      chilren inside the tag*/}
+      <Header/>
       <main>
         <h2>Time to get started!</h2>
       </main>
--- 01-starting-project/src/index.jsx
+++ 01-starting-project/src/index.jsx
@@ -3,5 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+// Root element is in index.html with id = root
 const entryPoint = document.getElementById("root");
+// createRoot set the element as a root and injects the App components and its components into its div
 ReactDOM.createRoot(entryPoint).render(<App />);
