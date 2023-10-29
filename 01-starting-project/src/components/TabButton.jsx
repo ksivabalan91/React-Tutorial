3e08a7098d393767777bc13b80e4edb9aa37860b
ksivabalan <ksivabalan.91@gmail.com>
2023-10-29 14:34:42 +0800

    Add onClick event listener

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,6 +1,16 @@
 export default function TabButton(props) {
+
+    function handleClick(){
+        console.log('hello world')
+    }
+
     // children prop refers to the content between you tags
     return (
-        <li><button>{props.children}</button></li>
+        <li>
+            {/* add event listener to handle click events */}
+            {/* u need to pass in a function to handle the click */}
+            {/* If you using "handleClick()" it will run the function upon loading the page */}
+            <button onClick={handleClick}>{props.children}</button>
+        </li>
     )
 }
