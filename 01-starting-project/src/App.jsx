992459c31a0d3a0f279794dcab70d955e480c973
ksivabalan <ksivabalan.91@gmail.com>
2023-10-29 13:29:11 +0800

    import img as a file to using in jsx

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,3 +1,6 @@
+// import the image into the file
+import reactImg from './assets/react-core-concepts.png'
+
 const reactDescriptions = ['Fundamental','Crucial','Core'];
 
 function getRandomInt(max){
@@ -8,7 +11,7 @@ function Header() {
   const description = getRandomInt(2);
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {reactDescriptions[description]} React concepts you will need for almost any app you are
